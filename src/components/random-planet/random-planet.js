1a0ae216aa14dcec68615ec94ad238cd96367fd9
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-09-11 23:09:28 +0300

    transform data api

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,25 +1,64 @@
 import React, { Component } from "react";
 
+import SwapiService from "../../services/swapi-service";
+
 import "./random-planet.css";
 
 export default class RandomPlanet extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+  };
+
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  }
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25) + 2;
+    
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded);
+  }
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
 
   render() {
+    const {
+      planet: { id, planetName, population, rotationPeriod, diameter },
+    } = this.state;
+    const photo = `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`;
+    const noPhoto = "../../img/no_photo.png";
+
     return (
-      <div className='random-planet d-flex jumbotron'>
+      <div className='random-planet d-flex jumbotron rounded'>
         <div className='image-wrap'>
           <img
             className='planet-image'
-            src='https://starwars-visualguide.com/assets/img/planets/5.jpg'
-            alt='Dagobah'
+            src={photo}
+            alt={planetName}
           />
         </div>
         <div className='planet-description'>
-          <h4>Planet Name</h4>
-          <ul className='list-group'>
-            <li >Population: 1 000 000</li>
-            <li>Rotation Period: 23 days </li>
-            <li>Diameter: 8,900km</li>            
+          <h4>{planetName}</h4>
+          <ul className='list-group list-group-flush'>
+            <li className='list-group-item'>
+              <span className='term'>Population:</span>
+              <span>{population}</span>
+            </li>
+            <li className='list-group-item'>
+              <span className='term'>Rotation Period:</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className='list-group-item'>
+              <span className='term'>Diameter:</span>
+              <span>{diameter}</span>
+            </li>
           </ul>
         </div>
       </div>
