883e6e1a12dcde2c33f2043393e8cf1164c24250
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-09-11 13:34:06 +0300

    add base structure components

---
+++ src/components/starship-details/starship-details.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./starship-details.css";
+
+const StarshipDetails = () => {
+  return <div className='starship-details'></div>;
+};
+
+export default StarshipDetails;
