faef88ec10584dcf550c2b4b472f4231fe39eb64
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-09-16 14:13:53 +0300

    modify errorIndicator

--- src/components/blocks/ErrorIndicator/component.jsx
+++ src/components/blocks/ErrorIndicator/component.jsx
@@ -4,12 +4,14 @@ import "./style.css";
 import icon from "../../../assets/death-star.svg";
 
 const ErrorIndicator = () => {
-  return (<div className='error-indicator'>
-    <img src={icon} alt="error icon"></img>
-    <span className="boom">Boom!</span>
-    <span>something has gone terribly wrong</span>
-    <span>(but we already sent droids to fix it)</span>
-  </div>);
+  return (
+    <div className='error-indicator'>
+      <img src={icon} alt="error icon"></img>
+      <span className="boom">Boom!</span>
+      <span>something has gone terribly wrong</span>
+      <span>(but we already sent droids to fix it)</span>
+    </div>
+  );
 }
 
 export default ErrorIndicator;
