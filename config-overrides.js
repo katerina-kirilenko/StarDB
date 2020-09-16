6b03789848936c746c95729377289bbe383022a6
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-09-16 19:58:48 +0300

    fix alias

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,16 @@
-const {
-  override,
-  fixBabelImports,
-  addLessLoader,
-  addWebpackAlias,
-} = require("customize-cra");
-const path = require('path');
+const { override, addWebpackResolve } = require("customize-cra");
+const path = require("path");
 
 module.exports = override(
-  // fixBabelImports("import", {
-  //   libraryName: "antd",
-  //   libraryDirectory: "es",
-  //   style: true,
-  // }),
-  // addLessLoader({
-  //   javascriptEnabled: true,
-  //   modifyVars: theme,
-  // }),
-  addWebpackAlias({
-    ["@components"]: path.resolve(__dirname, "./src/components"),
-    ["@assets"]: path.resolve(__dirname, "./src/assets"),
-    ["@api"]: path.resolve(__dirname, "./src/api"),
+  addWebpackResolve({
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@api": path.resolve(__dirname, "./src/api"),
+
+      // for .css
+      images: path.resolve(__dirname, "./src/assets"),
+    },
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".css"],
   })
 );
-
